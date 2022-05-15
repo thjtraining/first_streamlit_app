156d8d837b012fba0e439eaad2db37fb976d610b
thjtraining <105587703+thjtraining@users.noreply.github.com>
2022-05-15 11:56:11 +0100

    Update streamlit_app.py
    fixed typo in spelling of omega

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omege 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
