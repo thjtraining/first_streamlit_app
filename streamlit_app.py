3ddecc6f813c2e2012912146c475dacab7ae2b03
thjtraining <105587703+thjtraining@users.noreply.github.com>
2022-05-17 18:23:10 +0100

    Update streamlit_app.py
    Remove extraneous information in fruityvice_response get address

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruit_chose = streamlit.text_input('What fruit would you like information about?
 streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalise it
 fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
