e559225ae7c424c076d26bd05092950774fd79f0
thjtraining <105587703+thjtraining@users.noreply.github.com>
2022-05-17 21:41:56 +0100

    Update streamlit_app.py
    Move the Fruityvice Code into a Try-Except (with a nested If-Else)

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title("My Mom's New Healthy Diner")
-
+  
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -26,17 +26,19 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalised)
+  streamlit.write('The user entered', fruit_choice)
 
+except URLError as e:
+    streamlit.error()
 # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the json version of the response and normalise it
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
-# import snowflake.connector
-# output the screen as a table
-streamlit.dataframe(fruityvice_normalised)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
